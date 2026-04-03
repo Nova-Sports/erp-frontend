677f28d603e0f895d738f0e02a07632152af6b55
tariq <tariq.dev43@gmail.com>
2026-04-03 22:26:50 +0500

    feat: enhance Vite configuration to load environment variables dynamically

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    server: {
+      host: env.VITE_HOST || "127.0.0.1",
+      port: parseInt(env.VITE_PORT) || 3001,
+    },
+  };
 });
